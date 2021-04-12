31dae58617431b99af359ff80fd908149f1078a8
Dyno Fu <dfu@salesforce.com>
2021-04-11 21:53:28 -0700

    init

---
+++ Makefile
@@ -0,0 +1,15 @@
+SHELL = /bin/bash
+
+NAMESPACE := default
+POD := amazonlinux
+
+bash:
+	kubectl exec -it --namespace $(NAMESPACE) $(POD) -- bash
+
+exec-make:
+	kubectl cp --namespace $(NAMESPACE) $(POD).mk $(POD):/tmp/Makefile
+	kubectl cp --namespace $(NAMESPACE) pyproject.toml $(POD):/tmp/
+	kubectl cp --namespace $(NAMESPACE) poetry.toml $(POD):/tmp/
+	kubectl exec -it --namespace $(NAMESPACE) $(POD) -- bash -c 'make -C /tmp install-virtualenv create-hadoop-user'
+	kubectl exec -it --namespace $(NAMESPACE) $(POD) -- bash -c 'sudo -S -u hadoop make -C /tmp install-python-packages tar-python-packages'
+	kubectl cp --namespace $(NAMESPACE) $(POD):/tmp/python3-user-packages.tar.gz ./python3-user-packages.tar.gz
---
+++ amazonlinux.mk
@@ -0,0 +1,36 @@
+SHELL = /bin/bash
+
+install-packages:
+	# tar and make needs to be installed before anything else.
+	yum install --assumeyes \
+		gzip            \
+		make            \
+		python3         \
+		python3-devel   \
+		shadow-utils    \
+		sudo            \
+		tar             \
+		util-linux      \
+		vim             \
+		which           \
+		zstd            \
+	# END
+
+create-hadoop-user:
+	# same as on EMR
+	id hadoop || { groupadd --gid 1001 hadoop && useradd --uid 1001 --gid 1001 --home-dir /home/hadoop hadoop; }
+	echo 'hadoop ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/hadoop
+
+# as root
+install-virtualenv: install-packages
+	pip3 install --upgrade pip
+	/usr/local/bin/pip3 install --user poetry
+	[[ ! -f /tmp/requirements.txt ]] && ~/.local/bin/poetry install --no-root || ~/.local/bin/poetry update
+	~/.local/bin/poetry export -f requirements.txt --output requirements.txt
+
+# as hadoop
+install-python-packages:
+	/usr/local/bin/pip3 install --user -r /tmp/requirements.txt
+
+tar-python-packages:
+	tar zcvf /tmp/python3-user-packages.tar.gz -C /home/hadoop .local
